c58a8c7bf0f4dc3bdee8e7455a99f89760c85b0d
Kingsley Victor <javaprodigy56@gmail.com>
2019-11-11 11:58:19 +0100

    feat(auth): user should be able to login with credentials  ``  [Finishes #169669198]

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import db from '../db';
 import helpers from '../helpers';
 
@@ -20,6 +21,7 @@ export default class AuthController {
         id: user.id,
         email: user.email,
         hash_id: user.hash_id,
+        username: user.username,
         token: Jwt.encode({
           id: user.id,
           password: user.password,
@@ -28,6 +30,98 @@ export default class AuthController {
       };
       res.status(201).json({
         statusCode: 201,
+        body: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        statusCode: 500,
+        body: error
+      });
+    }
+  }
+
+  /**
+   *
+   * @param {Request} req
+   * @param {Response} res
+   */
+  static async loginWithUsername(req, res) {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findByUsername(username);
+      if (!user) {
+        res.status(404).json({
+          statusCode: 404,
+          body: `User with username ${username} not found`
+        });
+        return;
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        res.status(400).json({
+          statusCode: 400,
+          body: 'Incorrect password'
+        });
+        return;
+      }
+      const data = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        hash_id: user.hash_id,
+        token: Jwt.encode({
+          id: user.id,
+          hash_id: user.hash_id,
+          password: user.password
+        })
+      };
+      res.status(200).json({
+        statusCode: 200,
+        body: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        statusCode: 500,
+        body: error
+      });
+    }
+  }
+
+  /**
+   *
+   * @param {Request} req
+   * @param {Response} res
+   */
+  static async loginWithEmail(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findByEmail(email);
+      if (!user) {
+        res.status(404).json({
+          statusCode: 404,
+          body: `User with email ${email} not found`
+        });
+        return;
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        res.status(400).json({
+          statusCode: 400,
+          body: 'Incorrect password'
+        });
+        return;
+      }
+      const data = {
+        id: user.id,
+        username: user.username,
+        hash_id: user.hash_id,
+        email: user.email,
+        token: Jwt.encode({
+          id: user.id,
+          hash_id: user.hash_id,
+          password: user.password
+        })
+      };
+      res.status(200).json({
+        statusCode: 200,
         body: data
       });
     } catch (error) {
